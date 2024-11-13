bc3c1f96a9602db96654563a63e7b69545adde04
Guilherme R. de Oliveira <guilhermerl.dev@gmail.com>
2024-11-12 22:44:17 -0300

    Import db, userRoutes and add express methods to handle with body data

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,13 @@
 const express = require("express");
 const dotenv = require("dotenv").config();
 const port = process.env.PORT || 5000;
+const connectionDB = require("./config/db");
+const userRoutes = require("./routes/userRoutes");
 const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+app.use("/api/users", userRoutes);
 
 app.listen(port, () => console.log(`Server started on port ${port}`));
