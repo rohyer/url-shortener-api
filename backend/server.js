0d73f007e580c263d0ea5889d658e802cd28f4ba
Guilherme R. de Oliveira <guilhermerl.dev@gmail.com>
2024-11-13 11:33:07 -0300

    Change CommonJS to ESM and add /api/urls routes

--- backend/server.js
+++ backend/server.js
@@ -1,13 +1,15 @@
-const express = require("express");
-const dotenv = require("dotenv").config();
-const port = process.env.PORT || 5000;
-const connectionDB = require("./config/db");
-const userRoutes = require("./routes/userRoutes");
+import express from "express";
+import dotenv from "dotenv/config.js";
+import getConnection from "./config/db.js";
+import userRoutes from "./routes/userRoutes.js";
+import urlRoutes from "./routes/urlRoutes.js";
 const app = express();
+const port = process.env.PORT || 5000;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
 app.use("/api/users", userRoutes);
+app.use("/api/urls", urlRoutes);
 
 app.listen(port, () => console.log(`Server started on port ${port}`));
