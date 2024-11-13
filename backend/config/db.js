1fd45d7252300f2fd6923743bf533d6893b071ea
Guilherme R. de Oliveira <guilhermerl.dev@gmail.com>
2024-11-12 21:32:42 -0300

    Connect to database url_shortener using mysql2 module

---
+++ backend/config/db.js
@@ -0,0 +1,30 @@
+const mysql = require("mysql2");
+
+let pool;
+
+const connectionDB = () => {
+  try {
+    pool = mysql.createPool({
+      host: "localhost",
+      user: "root",
+      password: "",
+      database: "url_shortener"
+    });
+
+    console.log("MySQL conectado!");
+  } catch (error) {
+    console.log(`Erro ao conectar com o banco de dados: ${error}`);
+    process.exit(1);
+  }
+};
+
+const getConnection = () => {
+  if (!pool) {
+    throw new Error("Por favor, inicie o Banco de dado primeiro!");
+  }
+  return pool.promise();
+};
+
+connectionDB();
+
+module.exports = getConnection;
