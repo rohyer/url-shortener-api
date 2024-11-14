107a17ad71cecc473ec3567fb417a83c087eee56
Guilherme R. de Oliveira <guilhermerl.dev@gmail.com>
2024-11-14 15:45:01 -0300

    Import and use errorHandler

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,6 @@
 import express from "express";
 import dotenv from "dotenv/config.js";
+import { errorHandler } from "./middleware/errorMiddleware.js";
 import getConnection from "./config/db.js";
 import userRoutes from "./routes/userRoutes.js";
 import urlRoutes from "./routes/urlRoutes.js";
@@ -11,5 +12,7 @@ app.use(express.urlencoded({ extended: false }));
 
 app.use("/api/users", userRoutes);
 app.use("/api/urls", urlRoutes);
+
+app.use(errorHandler);
 
 app.listen(port, () => console.log(`Server started on port ${port}`));
